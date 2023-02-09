dca2302e56cd811d46030593bb0e0f7d258661e3
Rahuboy <rahulramach3000@gmail.com>
2023-02-09 13:34:44 +0530

    Pushing all sql files for loading data (note: csv files will have to be downloaded)

---
+++ backend/db/data.sql
@@ -0,0 +1,59 @@
+-- The csv files should be in a directory called 'csv' in the same directory as this file. Otherwise, the path might have to be changes
+
+LOAD DATA LOCAL INFILE './csv/Badges.csv' 
+INTO TABLE badges 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+LOAD DATA LOCAL INFILE './csv/Comments.csv' 
+INTO TABLE comments 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+LOAD DATA LOCAL INFILE './csv/PostHistory.csv' 
+INTO TABLE post_history 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+LOAD DATA LOCAL INFILE './csv/PostLinks.csv' 
+INTO TABLE post_links 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+LOAD DATA LOCAL INFILE './csv/Posts.csv' 
+INTO TABLE posts 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+LOAD DATA LOCAL INFILE './csv/Tags.csv' 
+INTO TABLE tags 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+LOAD DATA LOCAL INFILE './csv/Users.csv' 
+INTO TABLE users 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+LOAD DATA LOCAL INFILE './csv/Votes.csv' 
+INTO TABLE votes 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+-- use source data.sql to dump data
---
+++ backend/db/drop.sql
@@ -0,0 +1,11 @@
+-- drop tables
+drop table if exists badges; 
+drop table if exists comments; 
+drop table if exists post_history; 
+drop table if exists post_links; 
+drop table if exists posts; 
+drop table if exists tags; 
+drop table if exists users; 
+drop table if exists votes;
+
+-- use source drop.sql to drop tables
