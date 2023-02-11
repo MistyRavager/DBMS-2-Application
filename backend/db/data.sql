eb778f07a3747ee3129be41ac07f5e690dd522dd
Suryaansh-Jain <suryaansh2003@gmail.com>
2023-02-11 21:26:36 +0530

    added some code

--- backend/db/data.sql
+++ backend/db/data.sql
@@ -1,59 +1,44 @@
 -- The csv files should be in a directory called 'csv' in the same directory as this file. Otherwise, the path might have to be changes
 
-LOAD DATA LOCAL INFILE './csv/Badges.csv' 
-INTO TABLE badges 
-FIELDS TERMINATED BY ',' 
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+DELETE FROM badges;
+DELETE FROM comments;
+DELETE FROM post_history;
+DELETE FROM post_links;
+DELETE FROM posts;
+DELETE FROM tags;
+DELETE FROM users;
+DELETE FROM votes;
 
+LOAD XML LOCAL INFILE '/Users/suryanshjain/Desktop/xml/PostHistory1.xml'
+INTO TABLE post_history
+ROWS IDENTIFIED BY '<row>';
 
-LOAD DATA LOCAL INFILE '/Users/suryanshjain/Desktop/DBMS-2-Application_local/csv/Comments.csv' 
-INTO TABLE comments 
-FIELDS TERMINATED BY ',' 
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+LOAD XML LOCAL INFILE '/Users/suryanshjain/Desktop/xml/PostLinks1.xml'
+INTO TABLE post_links
+ROWS IDENTIFIED BY '<row>';
 
+LOAD XML LOCAL INFILE '/Users/suryanshjain/Desktop/xml/Posts1.xml'
+INTO TABLE posts
+ROWS IDENTIFIED BY '<row>';
 
-LOAD DATA LOCAL INFILE './csv/PostHistory.csv' 
-INTO TABLE post_history 
-FIELDS TERMINATED BY ',' 
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+LOAD XML LOCAL INFILE '/Users/suryanshjain/Desktop/xml/Tags1.xml'
+INTO TABLE tags
+ROWS IDENTIFIED BY '<row>';
 
+LOAD XML LOCAL INFILE '/Users/suryanshjain/Desktop/xml/Users.xml'
+INTO TABLE users
+ROWS IDENTIFIED BY '<row>';
 
-LOAD DATA LOCAL INFILE './csv/PostLinks.csv' 
-INTO TABLE post_links 
-FIELDS TERMINATED BY ',' 
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+LOAD XML LOCAL INFILE '/Users/suryanshjain/Desktop/xml/Votes1.xml'
+INTO TABLE votes
+ROWS IDENTIFIED BY '<row>';
 
+LOAD XML LOCAL INFILE '/Users/suryanshjain/Desktop/xml/Badges1.xml'
+INTO TABLE badges
+ROWS IDENTIFIED BY '<row>';
 
-LOAD DATA LOCAL INFILE './csv/Posts.csv' 
-INTO TABLE posts 
-FIELDS TERMINATED BY ',' 
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-
-LOAD DATA LOCAL INFILE './csv/Tags.csv' 
-INTO TABLE tags 
-FIELDS TERMINATED BY ',' 
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-
-LOAD DATA LOCAL INFILE '/Users/suryanshjain/Desktop/DBMS-2-Application_local/csv/Users.csv' 
-INTO TABLE users 
-FIELDS TERMINATED BY ',' 
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-
-LOAD DATA LOCAL INFILE './csv/Votes.csv' 
-INTO TABLE votes 
-FIELDS TERMINATED BY ',' 
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
+LOAD XML LOCAL INFILE '/Users/suryanshjain/Desktop/xml/Comments1.xml'
+INTO TABLE comments
+ROWS IDENTIFIED BY '<row>';
 
 -- use source data.sql to dump data
--- backend/db/drop.sql
+++ backend/db/drop.sql
@@ -1,4 +1,5 @@
 -- drop tables
+
 drop table if exists badges; 
 drop table if exists comments; 
 drop table if exists post_history; 
@@ -7,5 +8,6 @@ drop table if exists posts;
 drop table if exists tags; 
 drop table if exists users; 
 drop table if exists votes;
+drop table if exists credentials;
 
 -- use source drop.sql to drop tables
