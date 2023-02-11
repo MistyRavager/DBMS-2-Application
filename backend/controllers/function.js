6dcd2003ea4a8ae264b8881f3502b3c337dc8bac
Suryaansh-Jain <suryaansh2003@gmail.com>
2023-02-11 20:43:57 +0530

    added some code

--- backend/controllers/function.js
+++ backend/controllers/function.js
@@ -77,3 +77,29 @@ export const AutocompleteTag = async (req, res) => {
     }
 };
 
+export const answerQuestion = async (req, res) => {
+    try {
+        let post_id = req.body.post_id;
+        let user_id = req.body.user_id;
+        let answer = req.body.answer;
+
+        const post = await Post.findOne({
+            where: {
+                id: post_id
+            }
+        });
+
+        if (post) {
+            const postHistory = await PostHistory.create({
+                post_id: post_id,
+                user_id: user_id,
+                comment: answer
+            });
+            res.status(200).json(postHistory);
+        } else {
+            res.status(404).json("Post not found");
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
