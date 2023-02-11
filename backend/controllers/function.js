236dbaea4ba7c933d3124a0863061600541b0aa9
Suryaansh-Jain <suryaansh2003@gmail.com>
2023-02-11 21:03:49 +0530

    added some code

--- backend/controllers/function.js
+++ backend/controllers/function.js
@@ -103,3 +103,139 @@ export const answerQuestion = async (req, res) => {
         res.status(500).json(error);
     }
 }
+
+export const getPostByID = async (req, res) => {
+    try {
+        let post_id = req.body.post_id;
+
+        const post = await Post.findOne({
+            where: {
+                id: post_id
+            }
+        });
+
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+export const getPostByUserID = async (req, res) => {
+    try {
+        let user_id = req.body.user_id;
+
+        const post = await Post.findAll({
+            where: {
+                owner_user_id: user_id
+            }
+        });
+
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+export const getPostByTag = async (req, res) => {
+    try {
+        let tag_name = req.body.tag_name;
+
+        const post = await Post.findAll({
+            where: {
+                tags: {
+                    [Sequelize.Op.like]: "%" + tag_name + "%"
+                }
+            }
+        });
+
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+// upvote question will also upvote the owner of the question
+export const upvoteQuestion = async (req, res) => {
+    try {
+        let post_id = req.body.post_id;
+        let user_id = req.body.user_id;
+
+        const post = await Post.findOne({
+            where: {
+                id: post_id
+            }
+        });
+
+        const owner = await User.findOne({
+            where: {
+                id: post.owner_user_id
+            }
+        });
+
+        if (post && owner) {
+            const vote = await Vote.create({
+                post_id: post_id,
+                user_id: user_id,
+                vote_type_id: 2
+            });
+
+            const nuserU = await User.update({
+                reputation: owner.reputation + 10,
+                up_vote: owner.up_vote + 1
+            }, {
+                where: {
+                    id: owner.id
+                }
+            });
+
+            res.status(200).json(vote);
+        } else {
+            res.status(404).json("Post not found");
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+// downvote question will also downvote the owner of the question
+export const downvoteQuestion = async (req, res) => {
+    try {
+        let post_id = req.body.post_id;
+        let user_id = req.body.user_id;
+
+        const post = await Post.findOne({
+            where: {
+                id: post_id
+            }
+        });
+
+        const owner = await User.findOne({
+            where: {
+                id: post.owner_user_id
+            }
+        });
+
+        if (post && owner) {
+            const vote = await Vote.create({
+                post_id: post_id,
+                user_id: user_id,
+                vote_type_id: 1
+            });
+
+            const nuserU = await User.update({
+                reputation: owner.reputation - 10,
+                up_vote: owner.up_vote - 1
+            }, {
+                where: {
+                    id: owner.id
+                }
+            });
+
+            res.status(200).json(vote);
+        } else {
+            res.status(404).json("Post not found");
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
