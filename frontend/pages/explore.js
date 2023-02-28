75fe4608ba073af96cd788f01b73706c13189eca
purplehand52 <drishit2003@gmail.com>
2023-02-28 18:36:16 +0530

    Rishit's first commit. I'm slacking and I'm gay.

--- frontend/pages/explore.js
+++ frontend/pages/explore.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import Sidebar from '../components/sidebar'
 import Head from 'next/head';
 import Avatar from '@mui/material/Avatar';
@@ -12,69 +12,101 @@ import Box from '@mui/material/Box';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Chip from '@mui/material/Chip';
+import AutoSearch from '../components/autoSearch';
+
+function devChip (arr, name, getTagProps, index) {
+  var result = arr.find(item => item.name == name)
+  return <Chip variant="outlined" label={result.id + " : " + name} {...getTagProps({ index })} color='primary'/>
+}
+
+// Tags
+const tags = [
+  { name: "Rust", id: 1},
+  { name: "C++", id: 2},
+  { name: "Java", id: 3},
+  { name: "Python", id: 4}
+]
+
+// Usernames
+const users = [
+  { name: "Rahul", id: 1 },
+  { name: "Rishit", id: 2},
+  { name: "Kushagra", id: 3},
+]
+
 
 export default function Explore() {
-    return (
-    <>
-    <Head>
-        <title>Explore</title>
-    </Head>
-    <Box sx={{ display: 'flex' }}>
-      <Sidebar/> 
-      {/* no login */}
-      <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-            // backgroundImage: 'url(https://source.unsplash.com/random)',
-            // backgroundRepeat: 'no-repeat',
-            // backgroundSize: 'cover',
-            // backgroundPosition: 'center',
-          }}
-        >
-          <Toolbar />
-          <Container maxWidth="xl" sx={{ mt: 0, mb: 4 }}>
-            <Grid container spacing={3}>
-              {/* Chart */}
-              <Grid item xs={12} >
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                    fontFamily: 'Roboto',
-                  }}
-                >
-                  <Typography variant="h4" component="div" gutterBottom> 
-                    What are you looking for?
-                  </Typography>
-
-                  {/* search bar */}
-                </Paper>
-              </Grid>
-              
-              <Grid item xs={6} sx={{ml:"25%"}}>
-                <Typography variant="h6" component="div" gutterBottom sx={{textAlign:'center'}}>
-                  Didn't find what you were looking for?
+  /* State */
+  const [formData, setFormData] = useState({})
+  console.log(formData)
+  return (
+  <>
+  <Head>
+      <title>Explore</title>
+  </Head>
+  <Box sx={{ display: 'flex' }}>
+    <Sidebar/> 
+    {/* no login */}
+    <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto',
+          // backgroundImage: 'url(https://source.unsplash.com/random)',
+          // backgroundRepeat: 'no-repeat',
+          // backgroundSize: 'cover',
+          // backgroundPosition: 'center',
+        }}
+      >
+        <Toolbar />
+        <Container maxWidth="xl" sx={{ mt: 0, mb: 4 }}>
+          <Grid container spacing={3}>
+            {/* Chart */}
+            <Grid item xs={12} >
+              <Paper
+                sx={{
+                  p: 2,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: 240,
+                  fontFamily: 'Roboto',
+                }}
+              >
+                <Typography variant="h4" component="div" gutterBottom sx={{marginLeft: "25%", width: "50%"}}> 
+                  What are you looking for?
                 </Typography>
-                {/* <Card variant="outlined" sx={{ p: 2, display: 'flex', flexDirection: 'column' }}> */}
-                    <Button variant="outlined" href="/signin" sx={{width:"100%"}}>
-                        Login to create a post
-                    </Button>
-                {/* </Card> */}
-              </Grid>
+
+                <Stack spacing={3} sx={{ ml: "25%", width: "50%" }}>
+                  <AutoSearch setDetails={setFormData}/>
+                </Stack>
+
+              </Paper>
             </Grid>
-            {/* <Copyright sx={{ pt: 4 }} /> */}
-          </Container>
-        </Box>
-    </Box>
-    </>
-    );
+            
+            <Grid item xs={6} sx={{ml:"25%"}}>
+              <Typography variant="h6" component="div" gutterBottom sx={{textAlign:'center'}}>
+                Didn't find what you were looking for?
+              </Typography>
+              {/* <Card variant="outlined" sx={{ p: 2, display: 'flex', flexDirection: 'column' }}> */}
+                  <Button variant="outlined" href="/signin" sx={{width:"100%"}}>
+                      Login to create a post
+                  </Button>
+              {/* </Card> */}
+            </Grid>
+          </Grid>
+          {/* <Copyright sx={{ pt: 4 }} /> */}
+        </Container>
+      </Box>
+  </Box>
+  </>
+  );
 }
